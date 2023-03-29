bb93b460f7d859efd42d55270a2f0e66a4441172
sceiiya <meetscheidj@gmail.com>
2023-03-30 04:08:46 +0800

    added suggest top product container include

---
+++ includes/right_top_suggest_side.php
@@ -0,0 +1,44 @@
+<section class="suggest-cards-container">
+    <div class="suggest-card-cont-attr">
+
+        <a class="suggest-card-attr" href="/all-products/Shalltear-Bloodfallen_t-shirt/" title="Shalltear Bloodfallen OVERLORD Anime Series inspired T-shirt">
+            <div class="suggest-item-img-cont">
+                <img src="/all-products/Shalltear-Bloodfallen_t-shirt/1.jpg" class="suggest-item-img-main" alt="">
+            </div>
+            <div class=" suggest-item-inf-cont">
+                <div class="suggest-item-inf-tex-cont">
+                    <p class="suggest-item-name txt-light-inv">
+                        Shalltear Bloodfallen OVERLORD Anime Series inspired T-shirt
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <a class="suggest-card-attr" href="/all-products/minato-aqua-scale-figurine/" title="Minato Aqua: Aqua Iro Super Dream Ver. 1/7 Scale Figure">
+            <div class="suggest-item-img-cont">
+                <img src="/all-products/minato-aqua-scale-figurine/1.jpg" class="suggest-item-img-main" alt="">
+            </div>
+            <div class=" suggest-item-inf-cont">
+                <div class="suggest-item-inf-tex-cont">
+                    <p class="suggest-item-name txt-light-inv">
+                        Minato Aqua: Aqua Iro Super Dream Ver. 1/7 Scale Figure
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <a class="suggest-card-attr"  href="/all-products/Enna-Alouette_t-shirt/" title="Enna Alouette NIJISANJI EN inspired T-shirt">
+            <div class="suggest-item-img-cont">
+                <img src="/all-products/Enna-Alouette_t-shirt/3.jpg" class="suggest-item-img-main" alt="">
+            </div>
+            <div class=" suggest-item-inf-cont">
+                <div class="suggest-item-inf-tex-cont">
+                    <p class="suggest-item-name txt-light-inv">
+                        Enna Alouette NIJISANJI EN inspired T-shirt
+                    </p>
+                </div>
+            </div>
+        </a>
+
+    </div>
+</section>
