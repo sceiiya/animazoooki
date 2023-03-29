c35525908cf9a70014f7b37231c43854c7bec3e2
sceiiya <meetscheidj@gmail.com>
2023-03-30 03:59:55 +0800

    created file and added main navbar include

--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 <html lang="en">
 
 <head>
-
+    <!-- head -->
     <title>Animazooki Merch Co. | Homepage</title>
 </head>
 
 <body class="bg-light">
 
+<!-- messenger -->
 
+<!-- back to top -->
 
+<!-- promo -->
 
-
-
-
-
+<!-- header -->
 
     <!-- main hero -->
     <div id="carouselExampleDark" class="carousel carousel-light slide carousel_animazooki_hero" data-bs-ride="carousel">
@@ -95,48 +95,7 @@
     <!-- main hero -->
 
     <!-- navigation bar heading -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top bg-light-in" style="top:29px">
-        <div class="container-fluid">
-            <a class="navbar-brand ps-5" href="#">
-                <!-- light-mode -->
-                <img src="/assets/img/n-logo-b.png" alt="Animazooki" style="max-height: 30px" class="pb-1">
-                <!-- dark-mode -->
-                <!-- <img src="/assets/img/n-logo-w.png" alt="" style="max-height: 35px"> -->
-            </a>
-            <button class="burger-toggler navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse bg-light-in" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active txt-light-inv" aria-current="page" href="#">Home &nbsp; </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link txt-light-inv" href="/vouchers/">Vouchers &nbsp; </a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle txt-light-inv" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop By &nbsp;</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item txt-light-inv" href="/all-products/category/">Category</a></li>
-                            <li><a class="dropdown-item txt-light-inv" href="/all-products/series/">Series</a></li>
-                            <li><a class="dropdown-item txt-light-inv" href="#" onclick="popdev()">Trend</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link txt-light-inv" href="#" onclick="popdev()">New Arrivals &nbsp; </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link txt-light-inv" href="#" onclick="popdev()">Upcoming Merchandise &nbsp; </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link txt-light-inv" href="/all-products/">All Products &nbsp; </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link txt-light-inv" href="/articles/">Articles &nbsp; </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+
     <!-- navigation bar heading -->
 
 
