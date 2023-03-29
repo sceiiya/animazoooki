1ac6b03cb8c3352c7f0d5f6216daf7c8baf55b9c
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-03-30 05:02:05 +0800

    added right bottom article container include

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <!-- head -->
+    
     <title>Animazooki Merch Co. | Homepage</title>
 </head>
 
@@ -1288,65 +1289,14 @@
 <!-- suggest top product container here -->
 
             </aside>
+
             <aside class="side-sec-b">
 
-                <section class="article-cards-container">
-                    <div class="article-card-cont-attr">
+<!-- suggest bottom article container here -->
 
-
-
-
-                        <a class="article-card-attr" href="/articles/Mushoku-Tensei-2-stage-at-Anime-Japan-2023-anime-news-3/" target="_blank" title="Mushoku Tensei 2 stage at Anime Japan 2023">
-                            <div class="article-item-img-cont">
-                                <img src="https://staticg.sportskeeda.com/editor/2023/02/871d9-16757938399220-1920.jpg" class="suggest-item-img-main" alt="Mushoku Tensei 2 stage at Anime Japan 2023">
-                            </div>
-                            <div class=" article-item-inf-cont">
-                                <div class="article-item-inf-tex-cont">
-                                    <p class="article-item-name txtred">
-                                        Mushoku Tensei 2 stage at Anime Japan 2023
-                                    </p>
-                                </div>
-
-                            </div>
-                        </a>
-
-                        <a class="article-card-attr" href="/articles/NIJISANJI-EN-Announces-Ethyria-anime-news-2/" target="_blank" title="NIJISANJI EN Announces 3rd Virtual YouTuber Group 'Ethyria'">
-                            <div class="article-item-img-cont">
-                                <img src="https://cdn.shopify.com/s/files/1/0577/1254/1891/files/y7DTwXkaDymgZyEA7Nkf.jpg?v=1664945493" class="suggest-item-img-main" alt="NIJISANJI EN Announces 3rd Virtual YouTuber Group 'Ethyria'">
-                            </div>
-                            <div class=" article-item-inf-cont">
-                                <div class="article-item-inf-tex-cont">
-                                    <p class="article-item-name txtred">
-                                        NIJISANJI EN Announces 3rd Virtual YouTuber Group "Ethyria"
-                                    </p>
-                                </div>
-
-                            </div>
-                        </a>
-
-                        <a class="article-card-attr" href="/articles/Tensura-Film-to-be-Released-Worldwde-anime-news-1/" target="_blank" title="That Time I Got Reincarnated as a Slime Film to be Released Worldwde">
-                            <div class="article-item-img-cont">
-                                <img src="https://cdn.animenewsnetwork.com/thumbnails/max500x600/encyc/A20736-2172185775.1528434403.jpg" class="suggest-item-img-main" alt="That Time I Got Reincarnated as a Slime Film to be Released Worldwde">
-                            </div>
-                            <div class=" article-item-inf-cont">
-                                <div class="article-item-inf-tex-cont">
-                                    <p class="article-item-name txtred">
-                                        That Time I Got Reincarnated as a Slime Film to be Released Worldwde
-                                    </p>
-                                </div>
-
-                            </div>
-                        </a>
-
-
-
-
-                    </div>
-                </section>
             </aside>
         </aside>
-        <!--right side of the main--->
-
+        <!--end of right side of the main--->
     </section>
 
 
