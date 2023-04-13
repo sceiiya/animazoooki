72a73c5e96c7035a831560dd44ebb420954be0cf
Jayfourth <fourthcesar@gmail.com>
2023-04-13 13:45:09 +0800

    updated profile page

---
+++ profile/content/mycart.php
@@ -0,0 +1,5 @@
+<div class="container pt-5 w-75 bg-light-in">
+    <h2>My Cart</h2>
+
+    <p>NO CONTENT YET</p>
+</div>
---
+++ profile/content/myprofile.php
@@ -0,0 +1,38 @@
+<div class="container pt-5 w-75 bg-light-in" id="dataField">
+    <h2>My Account</h2>
+    <form action="">
+        <div class="mb-3 mt-3 w-75 profile-data">
+            <label>Username</label>
+            <input type="text" class="form-control" id="firstName" placeholder="Username">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>First Name</label>
+            <input type="text" class="form-control" id="lastName" placeholder="First Name">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>Last Name</label>
+            <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>Email Address</label>
+            <input type="email" class="form-control" id="emailAdd" placeholder="example@email.com">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>Contact Number</label>
+            <input type="text" class="form-control" id="contactNo" placeholder="ex. +63 987 654 3210">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>Billing Address</label>
+            <input type="text" class="form-control" id="contactNo" placeholder="House No., Street Name, City, Country">
+        </div>
+        <div class="mb-3 w-75 profile-data">
+            <label>Bank Account</label>
+            <input type="text" class="form-control" id="contactNo" placeholder="Bank Name">
+        </div>
+
+    </form>
+
+</div>
+<br>
+<br>
+<span><button class="profileButton" id="editBtn">EDIT</button></span>
