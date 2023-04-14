43fde64860b879af1fa9e208a0e1a51cec035e1d
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-04-14 16:46:31 +0800

    fixed some styles.. and added readonly attr45

--- profile/content/myprofile.php
+++ profile/content/myprofile.php
@@ -3,31 +3,31 @@
     <form action="">
         <div class="mb-3 mt-3 w-75 profile-data">
             <label>Username</label>
-            <input type="text" class="form-control" id="userName" placeholder="Username">
+            <input type="text" class="form-control" id="userName" placeholder="Username" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>First Name</label>
-            <input type="text" class="form-control" id="firstName" placeholder="First Name">
+            <input type="text" class="form-control" id="firstName" placeholder="First Name" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>Last Name</label>
-            <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+            <input type="text" class="form-control" id="lastName" placeholder="Last Name" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>Email Address</label>
-            <input type="email" class="form-control" id="emailAdd" placeholder="example@email.com">
+            <input type="email" class="form-control" id="emailAdd" placeholder="example@email.com" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>Contact Number</label>
-            <input type="text" class="form-control" id="contactNo" placeholder="ex. +63 987 654 3210">
+            <input type="text" class="form-control" id="contactNo" placeholder="ex. +63 987 654 3210" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>Billing Address</label>
-            <input type="text" class="form-control" id="billingAdd" placeholder="House No., Street Name, City, Country">
+            <input type="text" class="form-control" id="billingAdd" placeholder="House No., Street Name, City, Country" readonly>
         </div>
         <div class="mb-3 w-75 profile-data">
             <label>Bank Account</label>
-            <input type="text" class="form-control" id="bankAcc" placeholder="Bank Name">
+            <input type="text" class="form-control" id="bankAcc" placeholder="Bank Name" readonly>
         </div>
 
     </form>
