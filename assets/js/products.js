f7b3928ba9aea9bc61cdce3912e7d317dba30f25
sceiiya <meetscheidj@gmail.com>
2023-04-16 06:23:55 -0400

    product image done.. only USADA will work

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,70 +1,124 @@
 const imgMainCont = document.querySelector(".product-img-cont");
-const imgMainElm = document.querySelector(".product-img-main");
+const imgMainElm = document.querySelector(".product-img-cont img");
 const imgSub = document.querySelector(".product-img-sub");
 const imgSubCont = document.querySelector(".product-img-sub-cont-holder");
 
 const ZOOM = 300;
 
-imgMainCont.addEventListener("mouseenter", ()=>{
-    imgMainElm.style.width = ZOOM + '%';
-});
+// debugged
+let isMoving = false;
+let animationFrame;
 
-imgMainCont.addEventListener("mouseleave", ()=>{
-    imgMainElm.style.width = '100%';
-    imgMainElm.style.top = '0';
-    imgMainElm.style.left = '0';
-});
+function updateImage(xPos, yPos) {
+  let obj = imgMainCont;
+  let obj_left = 0;
+  let obj_top = 0;
 
-imgMainCont.addEventListener("mousemove", (mouseEvent)=>{
-    let obj = imgMainCont;
-    let obj_left = 0;
-    let obj_top = 0;
-    let xPos;
-    let yPos;
+  while (obj.offsetParent) {
+    obj_left += obj.offsetLeft;
+    obj_top += obj.offsetTop;
+    obj = obj.offsetParent;
+  }
 
-    while(obj.offsetParent){
-        obj_left += obj.offsetLeft;
-        obj_top += obj.offsetTop;
-        obj = obj.offsetParent
-        console.log("obj_left: " + obj_left);
-        console.log("obj_top: " + obj_top);
-    }
+  xPos -= obj_left;
+  yPos -= obj_top;
 
-    if(mouseEvent){
-        xPos = mouseEvent.pageX;
-        yPos = mouseEvent.pageY;
-        console.log(mouseEvent);
-        console.log("xPos: " + xPos);
-        console.log("yPos: " + yPos);
-    }else{
-        xPos = window.event.x + document.body.scrollLeft - 2;
-        yPos = window.event.y + document.body.scrollTop - 2;
-        console.log("xPos: " + xPos);
-        console.log("yPos: " + yPos);
-    }
+  const imgWidth = imgMainElm.clientWidth;
+  const imgHeight = imgMainElm.clientHeight;
 
-    xPos -= obj_left;
-    yPos -= obj_top;
-
-    const imgWidth = imgMainElm.clientWidth;
-    const imgHeight = imgMainElm.clientHeight;
-
-    imgMainElm.style.top = -(((imgHeight - this.clientHeight) * yPos) / this.clientHeight) + "px";
-    // imgMainElm.style.left = -(((imgWidth - this.imgWidth) * xPos) / this.imgWidth) + "px";
-});
-
-Array.from(imgSubCont.children).forEach((prodImg, i, list) =>{
-    prodImg.addEventListener("click", ()=>{
-        const newSrc = prodImg.querySelector("img").src;
-        imgMainElm.src = newSrc;
-
-        list.forEach(prod => prod.classList.remove("active"));
-        prodImg.classList.add("active");
-    });
-});
-
-function cngeHeight(){
-    imgMainCont.style.height = imgMainCont.clientHeight + 'px';
+  imgMainElm.style.top = -(((imgHeight - imgMainCont.clientHeight) * yPos) / imgMainCont.clientHeight) + "px";
+  imgMainElm.style.left = -(((imgWidth - imgMainCont.clientWidth) * xPos) / imgMainCont.clientWidth) + "px";
 }
+
+function onMouseMove(mouseEvent) {
+  if (!isMoving) {
+    isMoving = true;
+    animationFrame = requestAnimationFrame(() => {
+      updateImage(mouseEvent.pageX, mouseEvent.pageY);
+      isMoving = false;
+    });
+  }
+}
+
+function onMouseLeave() {
+  imgMainElm.style.width = '100%';
+  imgMainElm.style.top = '0';
+  imgMainElm.style.left = '0';
+}
+
+imgMainCont.addEventListener("mouseenter", () => {
+  imgMainElm.style.width = ZOOM + '%';
+});
+
+imgMainCont.addEventListener("mouseleave", onMouseLeave);
+
+imgMainCont.addEventListener("mousemove", onMouseMove);
+
+//origin
+// imgMainCont.addEventListener("mouseenter", () => {
+//   imgMainElm.style.width = ZOOM + '%';
+// });
+
+// imgMainCont.addEventListener("mouseleave", () => {
+//   imgMainElm.style.width = '100%';
+//   imgMainElm.style.top = '0';
+//   imgMainElm.style.left = '0';
+// });
+
+// imgMainCont.addEventListener("mousemove", (mouseEvent) => {
+//   let obj = imgMainCont;
+//   let obj_left = 0;
+//   let obj_top = 0;
+//   let xPos;
+//   let yPos;
+
+//   while (obj.offsetParent) {
+//     obj_left += obj.offsetLeft;
+//     obj_top += obj.offsetTop;
+//     obj = obj.offsetParent;
+//   }
+
+//   if (mouseEvent) {
+//     xPos = mouseEvent.pageX;
+//     yPos = mouseEvent.pageY;
+//   } else {
+//     xPos = window.event.x + document.body.scrollLeft - 2;
+//     yPos = window.event.y + document.body.scrollTop - 2;
+//   }
+
+//   xPos -= obj_left;
+//   yPos -= obj_top;
+
+//   const imgWidth = imgMainElm.clientWidth;
+//   const imgHeight = imgMainElm.clientHeight;
+
+// imgMainElm.style.top = -(((imgHeight - imgMainCont.clientHeight) * yPos) / imgMainCont.clientHeight) + "px";
+// imgMainElm.style.left = -(((imgWidth - imgMainCont.clientWidth) * xPos) / imgMainCont.clientWidth) + "px";
+
+// // imgMainElm.style.top = -(((imgHeight - this.clientHeight) * yPos) / this.clientHeight) + "px ";
+// // imgMainElm.style.left = -(((imgWidth - this.clientWidth) * xPos) / this.clientWidth) + "px ";
+  
+
+// //   imgMainElm.style.transform = `trsndlateY({-(((imgHeight - imgMainCont.clientHeight) * yPos) / imgMainCont.clientHeight)}px)`;
+//   //   imgMainElm.style.top = -(((imgHeight - this.clientHeight) * yPos) / this.clientHeight) + "px ";
+//     // imgMainElm.style.transform = `trsndlateX({-(((imgWidth - this.clientWidth) * xPos) / this.clientWidth)}px)`;
+  
+// });
+
+
+Array.from(imgSubCont.children).forEach((productElm, i, list) => {
+  productElm.addEventListener("click", () => {
+    const newSrc = productElm.querySelector("img").src;
+    imgMainElm.src = newSrc;
+
+    list.forEach(prod => prod.classList.remove("active"));
+    productElm.classList.add("active");
+  });
+});
+
+function cngeHeight() {
+  imgMainCont.style.height = imgMainCont.clientHeight + 'px';
+}
+
 cngeHeight();
 window.addEventListener('resize', cngeHeight);
