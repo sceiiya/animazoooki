8c9071725a93dfb49d7a41aad6853490cb05b91e
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-04-16 14:53:12 +0800

    devloping magnify effect in product page

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,23 +1,19 @@
 const imgMainCont = document.querySelector(".product-img-cont");
-const imgMainElm = document.querySelector(".product-img-cont img");
+const imgMainElm = document.querySelector(".product-img-main");
 const imgSub = document.querySelector(".product-img-sub");
 const imgSubCont = document.querySelector(".product-img-sub-cont-holder");
 
 const ZOOM = 300;
 
-
-
-imgMainCont.addEventListener("mouseenter", function(){
+imgMainCont.addEventListener("mouseenter", ()=>{
     imgMainElm.style.width = ZOOM + '%';
 });
-
 
 imgMainCont.addEventListener("mouseleave", ()=>{
     imgMainElm.style.width = '100%';
     imgMainElm.style.top = '0';
     imgMainElm.style.left = '0';
 });
-
 
 imgMainCont.addEventListener("mousemove", (mouseEvent)=>{
     let obj = imgMainCont;
@@ -30,14 +26,21 @@ imgMainCont.addEventListener("mousemove", (mouseEvent)=>{
         obj_left += obj.offsetLeft;
         obj_top += obj.offsetTop;
         obj = obj.offsetParent
+        console.log("obj_left: " + obj_left);
+        console.log("obj_top: " + obj_top);
     }
 
     if(mouseEvent){
         xPos = mouseEvent.pageX;
         yPos = mouseEvent.pageY;
+        console.log(mouseEvent);
+        console.log("xPos: " + xPos);
+        console.log("yPos: " + yPos);
     }else{
         xPos = window.event.x + document.body.scrollLeft - 2;
         yPos = window.event.y + document.body.scrollTop - 2;
+        console.log("xPos: " + xPos);
+        console.log("yPos: " + yPos);
     }
 
     xPos -= obj_left;
@@ -47,17 +50,16 @@ imgMainCont.addEventListener("mousemove", (mouseEvent)=>{
     const imgHeight = imgMainElm.clientHeight;
 
     imgMainElm.style.top = -(((imgHeight - this.clientHeight) * yPos) / this.clientHeight) + "px";
-    imgMainElm.style.left = -(((imgWidth - this.imgWidth) * xPos) / this.imgWidth) + "px";
+    // imgMainElm.style.left = -(((imgWidth - this.imgWidth) * xPos) / this.imgWidth) + "px";
 });
 
-
-Array.from(imgSubCont.children).forEach((productELm, i, list) =>{
-    productElm.addEventListener("click", ()=>{
-        const newSrc = productELm.querySelector("img").src;
+Array.from(imgSubCont.children).forEach((prodImg, i, list) =>{
+    prodImg.addEventListener("click", ()=>{
+        const newSrc = prodImg.querySelector("img").src;
         imgMainElm.src = newSrc;
 
         list.forEach(prod => prod.classList.remove("active"));
-        productElm.classList.add("active");
+        prodImg.classList.add("active");
     });
 });
 
