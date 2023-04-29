d0238dd1236c93f38855a8954f6ea4c2ea43e136
sceiiya <meetscheidj@gmail.com>
2023-04-29 11:14:20 -0400

    buckling up user otp experience

--- controllers/validate_OTP.php
+++ controllers/validate_OTP.php
@@ -9,15 +9,21 @@
         $eCon = $ConDB->NewCon();
         if ($eCon == true){
             // $newotp = ['otp_code' => rand(100000, 999999)];
+            // $of = ['username' => $_SESSION['username']];
+            $OTP = $_POST['otp'];
+            $timestamp =  $_SERVER["REQUEST_TIME"];  // record the current time stamp 
+            if(($timestamp - $_SESSION['time']) > 300)  // 300 refers to 300 seconds
+            
             $of = ['username' => $_SESSION['username']];
-            $valid = $ConDB->GSelect($eCon, 'clients', 'otp_code', $_POST['otp']);
+            $valid = $ConDB->GSelect($eCon, 'clients', $of, '', '');
             // $eUpdate = $ConDB->Update($eCon, 'clients', $newotp, $of);
-                if($valid == "true"){
-                    $upd = ['status' => 'active'];
+                // if($valid['otp_code'] == $_POST['otp']){
+                if($valid['otp_code'] == $OTP){
+                    $upd = ['status' => 'active', 'otp_code' => ''];
                     $eUpdate = $ConDB->Update($eCon, 'clients', $upd, $of);
-                    echo "otp-sent";
-                }elseif($valid == 'false'){
-
+                    echo "true";
+                }elseif(!($valid['otp_code'] == $OTP)){
+                    echo "false";
                 }else{
                     $_SESSION['error'] = $valid;
                     header("Location: error_logger.php");
