9139963733c472b1b720aa1dcc782a03625608e5
Jayfourth <fourthcesar@gmail.com>
2023-05-05 17:26:03 +0800

    updated csv and pdf

--- controllers/admin/csv_customers.php
+++ controllers/admin/csv_customers.php
@@ -20,41 +20,44 @@
     $qSelect = "SELECT * FROM $dbDatabase .`clients` ORDER BY `id` DESC";
     $eSelect = mysqli_query($dbConnection, $qSelect);
 
+    $ofile = fopen("../../reports/csv/customers.csv", "wa+");
+
+    fwrite($ofile, "ID, Name, Username, Email, Password, Contact No., Shipping Add., Billing Add.s, Country, Total Orders, Total Reviews, Theme, Status, Date Added, Date Modified, OTP Code, Date Last Login, Newsletter, Profile Image\n");
+    while($rows = mysqli_fetch_assoc($eSelect)) {
+        fwrite($ofile, '"'.$rows['id'].'","'.$rows['name'].'","'.$rows['username'].'","'.$rows['email'].'","'.$rows['password'].'","'.$rows['contactno'].'","'.$rows['default_shipping_address'].'","'.$rows['billing_address'].'","'.$rows['Country'].'","'.$rows['total_orders'].'","'.$rows['total_reviews'].'","'.$rows['theme'].'","'.$rows['status'].'","'.$rows['date_added'].'","'.$rows['date_modified'].'","'.$rows['otp_code'].'","'.$rows['date_last_login'].'","'.$rows['newsletter'].'","'.$rows['profile_img'].'"');
+        fwrite($ofile, "\n");
+    }
+    fclose($ofile);
+
+    require '../../vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
     if ($eSelect == true) {
         try {
-
-            $ofile = fopen("../../reports/csv/customers.csv", "wa+");
-
-            fwrite($ofile, "ID, Name, Username, Email, Password, Contact No., Shipping Add., Billing Add.s, Country, Total Orders, Total Reviews, Theme, Status, Date Added, Date Modified, OTP Code, Date Last Login, Newsletter, Profile Image\n");
-            while($rows = mysqli_fetch_assoc($eSelect)) {
-                fwrite($ofile, '"'.$rows['id'].'","'.$rows['name'].'","'.$rows['username'].'","'.$rows['email'].'","'.$rows['password'].'","'.$rows['contactno'].'","'.$rows['default_shipping_address'].'","'.$rows['billing_address'].'","'.$rows['Country'].'","'.$rows['total_orders'].'","'.$rows['total_reviews'].'","'.$rows['theme'].'","'.$rows['status'].'","'.$rows['date_added'].'","'.$rows['date_modified'].'","'.$rows['otp_code'].'","'.$rows['date_last_login'].'","'.$rows['newsletter'].'","'.$rows['profile_img'].'"');
-                fwrite($ofile, "\n");
-            }
-            fclose($ofile);
+            $mail = new PHPMailer(true);
 
             require_once('../../phpmailer/class.phpmailer.php');
 
                         $mail = new PHPMailer();
-                        
-                        $mail->IsSMTP();
-                        $mail->SMTPAuth 	= true;
-                    
-                        $mail->Host 	  = 'smtp.hostinger.com';
-                        include("../important/connect_Email.php");
-                        $mail->FromName   = "System Administrator";
-                        $mail->SMTPSecure = 'ssl';
-                        $mail->Port 	  = 465;                        
-                        
-                        $mail->AddAddress($admEmail, $admUsername);
-                        $mail->AddAttachment("../../reports/csv/customers.csv");
-                    
+
+                        $mail->isSMTP();
+                        $mail->Host       = 'smtp.hostinger.com';
+                        $mail->SMTPAuth   = true;                  
+                        include('../important/connect_Email.php');
+                        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                        $mail->Port       = 465;
+                        $mail->setFrom('support@animazoooki.wd49p.com', 'System Administrator');
+                        $mail->addAddress($admEmail, $admUsername);
+                        $mail->addAttachment('../../reports/csv/customers.csv');
+                        $mail->isHTML(true);
                         $mail->Subject = "Customers Report";
-                        $mail->Body = nl2br("
-                        See attached document for the report.
-                        
-                        ");
-                        
-                        $mail->IsHTML(true);
+                        $mail->Body    = nl2br("
+                                     See attached document for the report.
+                                    
+                                     ");
 
                         if(!$mail->Send()) {
                             echo "Email not sent!";
